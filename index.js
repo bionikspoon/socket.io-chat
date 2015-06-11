71e97a03d8ba4ebadc7824c84db4ab2578ee895b
Manu <bionikspoon@gmail.com>
2015-06-11 13:42:04 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('index    ', 'A user connected');
+  io.emit('CHAT_MESSAGE', {
+    date: Date.now(),
+    message: 'A user connected'
+  });
+
+  socket.on('disconnect', function () {
+    console.log('index    ', 'user disconnected');
+    io.emit('CHAT_MESSAGE', {
+      date: Date.now(),
+      message: 'A user disconnected'
+    });
+  });
+
+  socket.on('CHAT_MESSAGE', function (message) {
+    if (message && message.user && message.message) {
+      message.date = Date.now();
+      console.log('index    ', 'message: ', message);
+
+      io.emit('CHAT_MESSAGE', message);
+    }
+  });
+
+  socket.on('IS_TYPING', function (data) {
+    io.emit('IS_TYPING', data);
+  })
+
+});
+
+http.listen(3000, function () {
+  console.log('index    ', 'listening on *:3000');
+});
